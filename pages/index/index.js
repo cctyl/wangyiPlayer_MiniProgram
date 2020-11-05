5a177a41cd45a965c1800fa0d4624a7e20fc7f08
cctyl <2621460357@qq.com>
2020-11-05 15:40:16 +0800

    完成排行榜数据的请求与展示

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,8 @@ Page({
    */
   data: {
     bannerList: [], //轮播图列表
-    recommendSongList:[]  //推荐歌曲列表
+    recommendSongList: [], //推荐歌曲列表
+    topList: []
   },
 
   /**
@@ -15,6 +16,7 @@ Page({
    */
   onLoad: async function (options) {
 
+    //获取banner列表
     let bannerListData = await request('/banner', {
       type: 2
     });
@@ -23,11 +25,31 @@ Page({
       bannerList: bannerListData.banners
     });
 
-
-    let recommendData = await request('/personalized',{limit:5});
-
+    //获取推荐歌曲数据
+    let recommendData = await request('/personalized', { limit: 5 });
     this.setData({
       recommendSongList: recommendData.result
+    });
+
+
+    //获取歌曲排行榜数据
+    let ids = ["19723756","3778678","71385702","2884035","5201625538"]; //写死几个排行榜的id
+    //存放处理后的排行榜对象
+    let topArray =[];
+    for (let i=0;i<ids.length;i++){
+      //得到原始数据，数据量特别大
+      let tempData = await request('/playlist/detail',{id:ids[i]});
+      //转换成自己的对象
+      let myTop ={
+        name: tempData.playlist.name,
+        tracks: tempData.playlist.tracks.slice(0,3)
+      };
+
+      topArray.push(myTop);
+    }
+
+    this.setData({
+      topList : topArray
     });
   },
 
